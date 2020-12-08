6308a27ff7516fd67a6c1c011e8d7e46f226d61f
breadone <pradyun.setti@gmail.com>
2020-12-08 16:54:43 +1300

    made bench a function

--- bench.py
+++ bench.py
@@ -1,17 +1,19 @@
-current = 0
-count = 0
-
 def isPrime(num):
-    #prime = False
     for i in range(2, num//2):
         if num % i == 0:
             return False
         else:
             return True
 
-while True:
-    current = current + 1
-    
-    if isPrime(current):
-        count = count + 1
-        print("[", count, "]", current)
+def bench(): 
+    current = 0
+    count = 0
+
+    while True:
+        current = current + 1    
+        if isPrime(current):
+            count = count + 1
+            print("[",count,"]", current)
+
+bench()
+
