179db34bcb983b577828525e007910a4076feb80
breadone <pradyun.setti@gmail.com>
2020-12-08 17:14:04 +1300

    timed bench implemented

--- bench.py
+++ bench.py
@@ -1,3 +1,5 @@
+import time
+
 def isPrime(num):
     for i in range(2, num//2):
         if num % i == 0:
@@ -15,5 +17,25 @@ def bench():
             count = count + 1
             print("[",count,"]", current)
 
-bench()
+def timedbench(tEnd):
+    t = time.time() + (60 * tEnd)    
+    current = 0
+    count = 0
+    
+    while time.time() < t:
+        current = current + 1    
+        if isPrime(current):
+            count = count + 1
+            print("[",count,"]", current)
+
+print("select function:\n1. free run\n2. run for 5 mins\n3. run for custom amount\n4. run until 10 million")
+inp = input("choice: ")
+
+if inp == "1":
+    bench()
+if inp == "2":
+    timedbench(5)
+if inp == "3":
+    t = int(input("enter time in minutes: "))
+    timedbench(t)
 
